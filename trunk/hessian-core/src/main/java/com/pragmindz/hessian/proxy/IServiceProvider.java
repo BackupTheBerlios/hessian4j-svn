d8a27d3cd62c8fa54aaa375d2303bc63db79a023
rlaenen <rlaenen@e1dd08ab-974c-0410-ba12-9ed0590ca53f>
2008-05-15 11:50:56 +0000

    Removed file/folder

--- trunk/hessian-core/src/main/java/com/pragmindz/hessian/proxy/IServiceProvider.java
+++
@@ -1,6 +0,0 @@
-package com.pragmindz.hessian.proxy;
-
-public interface IServiceProvider
-{
-    public Object getService(String aServiceName) throws ServiceNotFound;
-}
--- trunk/hessian-core/src/main/java/com/pragmindz/hessian/proxy/ServiceNotFound.java
+++
@@ -1,5 +0,0 @@
-package com.pragmindz.hessian.proxy;
-
-public class ServiceNotFound extends Exception
-{
-}
